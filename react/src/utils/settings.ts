9660bbedd6720879c26181751d07dd62ac88317f
maxim ishkhanov <m.ishkhanov@nv.dunice.net>
2025-06-09 12:24:44 +0300

    type-settings.ts

--- react/src/utils/settings.ts
+++ react/src/utils/settings.ts
@@ -1,9 +1,31 @@
-export const loadSettings = () => {
+interface AppSettings {
+  theme: 'light' | 'dark';
+  requestDelay: number;
+  requestChanceToSuccess: number;
+}
+
+declare global {
+  interface Window {
+    appSettings: AppSettings;
+  }
+}
+
+export const loadSettings = (): void => {
   try {
     const rawSettings = localStorage.getItem('app-settings');
     if (rawSettings == null) throw new Error();
 
-    window.appSettings = JSON.parse(rawSettings);
+    const parsed = JSON.parse(rawSettings);
+
+    if (
+      typeof parsed.theme === 'string' &&
+      typeof parsed.requestDelay === 'number' &&
+      typeof parsed.requestChanceToSuccess === 'number'
+    ) {
+      window.appSettings = parsed;
+    } else {
+      throw new Error();
+    }
   } catch {
     window.appSettings = {
       theme: 'light',
@@ -13,7 +35,10 @@ export const loadSettings = () => {
   }
 };
 
-export const saveSettings = (key, settings) => {
-  window.appSettings[key] = settings;
+export const saveSettings = <K extends keyof AppSettings>(
+  key: K,
+  value: AppSettings[K]
+): void => {
+  window.appSettings[key] = value;
   localStorage.setItem('app-settings', JSON.stringify(window.appSettings));
 };
