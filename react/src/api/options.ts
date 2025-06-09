93468426522ec5db372c98d0c8ec14b3c77a11ab
maxim ishkhanov <m.ishkhanov@nv.dunice.net>
2025-06-09 10:44:05 +0300

    type-options.ts

--- react/src/api/options.ts
+++ react/src/api/options.ts
@@ -2,4 +2,6 @@ import { request } from './request';
 
 export const getFirstOptions = async () => request('/first');
 
-export const getSecondOptions = async (payload) => request('/second', payload);
+type SecondPayload = { id: 1 | 2 | 3 };
+
+export const getSecondOptions = async (payload: SecondPayload) => request('/second', payload);
