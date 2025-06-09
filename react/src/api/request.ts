27687ae2ece07aabbf326ef08f2599616ee5c5b5
maxim ishkhanov <m.ishkhanov@nv.dunice.net>
2025-06-09 10:42:21 +0300

    type-request.ts

--- react/src/api/request.ts
+++ react/src/api/request.ts
@@ -1,16 +1,18 @@
 import { delay } from '../utils';
 import { MOCK_FIST_OPTIONS, MOCK_SECOND_OPTIONS } from './mock';
 
-export const request = async (path, data) => {
+export const request = async (
+  path: '/first' | '/second',
+  data?: { id: 1 | 2 | 3 }
+) => {
   await delay(window.appSettings.requestDelay);
   const chanceToSuccess = Math.random();
   if (
-    path === '/first' &&
-    chanceToSuccess > window.appSettings.requestChanceToSuccess
-  ) {
+    path === '/first' && 
+    chanceToSuccess > window.appSettings.requestChanceToSuccess) {
     return Promise.resolve(MOCK_FIST_OPTIONS);
   }
-  if (path === '/second') {
+  if (path === '/second' && data && data.id) {
     return Promise.resolve(MOCK_SECOND_OPTIONS[data.id]);
   }
   return Promise.reject('Something went wrong...');
